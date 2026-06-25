b22d76636d79b42c91a824e99376a2cbabf7833e
matthis-k <matthis.kaelble@gmail.com>
2026-06-25 14:50:29 +0200

    fix: wrap phenix.overlays in perSystem

--- modules/flake-module.nix
+++ modules/flake-module.nix
@@ -1,9 +1,11 @@
 { ... }: {
-  phenix.overlays = [(final: prev: {
-    phenix = (prev.phenix or {}) // {
-      hello-nvim = final.writeShellScriptBin "hello-nvim" ''
-        echo "hello from nvim"
-      '';
-    };
-  })];
+  perSystem = { ... }: {
+    phenix.overlays = [(final: prev: {
+      phenix = (prev.phenix or {}) // {
+        hello-nvim = final.writeShellScriptBin "hello-nvim" ''
+          echo "hello from nvim"
+        '';
+      };
+    })];
+  };
 }
