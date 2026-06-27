019070abedca0a3afd7f2fb3ab59103c790ea21e
matthis-k <matthis.kaelble@gmail.com>
2026-06-27 02:10:17 +0200

    feat: add .tend.json, devShell with shellHook, flakeModules.default

--- modules/flake-module.nix
+++ modules/flake-module.nix
@@ -1,7 +1,9 @@
-{ ... }: {
-  perSystem = { ... }: {
+{ inputs, ... }: {
+  perSystem = { system, ... }: {
     phenix.overlays = [(final: prev: {
-      phenix = (prev.phenix or {}) // {};
+      phenix = (prev.phenix or {}) // {
+        nvim-nix = inputs.phenix-nvim.packages.${final.system}.nvim-nix;
+      };
     })];
   };
 }
