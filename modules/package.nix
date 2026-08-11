bf1a06ffa0ef199a8241523c46b11a11fd6dde86
Matthis Kälble <69082876+matthis-k@users.noreply.github.com>
2026-08-11 04:32:01 +0200

    fix: compose phenix.nvim with existing config

--- modules/package.nix
+++ modules/package.nix
@@ -41,7 +41,32 @@
           which-key-nvim
           nvim-web-devicons
           nui-nvim
-          phenix-nvim
+          {
+            name = "phenix-nvim";
+            data = phenix-nvim;
+            config = ''
+              local phenix = require("phenix")
+              phenix.setup()
+
+              local function map(lhs, rhs, desc)
+                vim.keymap.set("n", lhs, rhs, {
+                  desc = "Phenix: " .. desc,
+                  silent = true,
+                })
+              end
+
+              map("<leader>po", "<cmd>PhenixOpen<cr>", "open session")
+              map("<leader>pn", "<cmd>PhenixNew<cr>", "new session")
+              map("<leader>pp", "<cmd>PhenixPrompt<cr>", "prompt / focus composer")
+              map("<leader>pc", "<cmd>PhenixConfig<cr>", "configure session")
+              map("<leader>px", "<cmd>PhenixCancel<cr>", "cancel prompt")
+              map("<leader>pq", "<cmd>PhenixClose<cr>", "close session")
+
+              require("which-key").add({
+                { "<leader>p", group = "Phenix" },
+              })
+            '';
+          }
           snacks-nvim
           resession-nvim
           nvim-lspconfig
