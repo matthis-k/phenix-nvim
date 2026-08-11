391ab2d1ebebf85a5ff8a7e631490602b16a4ec3
Matthis Kälble <69082876+matthis-k@users.noreply.github.com>
2026-08-11 04:58:48 +0200

    nvim: use minimal Phenix sidebar setup

--- modules/package.nix
+++ modules/package.nix
@@ -45,26 +45,7 @@
             name = "phenix-nvim";
             data = phenix-nvim;
             config = ''
-              local phenix = require("phenix")
-              phenix.setup()
-
-              local function map(lhs, rhs, desc)
-                vim.keymap.set("n", lhs, rhs, {
-                  desc = "Phenix: " .. desc,
-                  silent = true,
-                })
-              end
-
-              map("<leader>po", "<cmd>PhenixOpen<cr>", "open session")
-              map("<leader>pn", "<cmd>PhenixNew<cr>", "new session")
-              map("<leader>pp", "<cmd>PhenixPrompt<cr>", "prompt / focus composer")
-              map("<leader>pc", "<cmd>PhenixConfig<cr>", "configure session")
-              map("<leader>px", "<cmd>PhenixCancel<cr>", "cancel prompt")
-              map("<leader>pq", "<cmd>PhenixClose<cr>", "close session")
-
-              require("which-key").add({
-                { "<leader>p", group = "Phenix" },
-              })
+              require("phenix").setup()
             '';
           }
           snacks-nvim
