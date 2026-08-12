bdabb43e0bec9a464cbe271c5d49fbd66c463480
Matthis Kälble <69082876+matthis-k@users.noreply.github.com>
2026-08-13 01:09:29 +0200

    refactor: define color preview API contract

--- pack/phenix/opt/phenix-color-preview/plugin/phenix-color-preview.lua
+++ pack/phenix/opt/phenix-color-preview/plugin/phenix-color-preview.lua
@@ -4,7 +4,14 @@ end
 vim.g.loaded_phenix_color_preview = true
 
 local feature = require("phenix.color_preview")
-require("phenix.frontend").register_api("color_preview", feature)
+require("phenix.frontend").register_api("color_preview", feature, {
+  contract = {
+    configure = "function",
+    is_open = "function",
+    close = "function",
+    toggle = "function",
+  },
+})
 
 vim.keymap.set("n", "<Plug>(phenix-color-preview-toggle)", feature.toggle, {
   desc = "Phenix: toggle color preview",
