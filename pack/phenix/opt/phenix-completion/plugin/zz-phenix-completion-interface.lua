e6500d07f574f59cc9b3adccad5672c262277561
Matthis Kälble <69082876+matthis-k@users.noreply.github.com>
2026-08-13 01:03:51 +0200

    refactor: remove legacy completion interface wrapper

--- pack/phenix/opt/phenix-completion/plugin/zz-phenix-completion-interface.lua
+++
@@ -1,8 +0,0 @@
-require("phenix.frontend").provide("completion", {
-  show = function()
-    return require("blink.cmp").show()
-  end,
-  hide = function()
-    return require("blink.cmp").hide()
-  end,
-})
