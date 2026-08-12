228cec021af85cfe772b9df27d033404097c8cb3
Matthis Kälble <69082876+matthis-k@users.noreply.github.com>
2026-08-13 01:03:00 +0200

    refactor: register session API

--- pack/phenix/opt/phenix-session/plugin/phenix-session.lua
+++ pack/phenix/opt/phenix-session/plugin/phenix-session.lua
@@ -3,25 +3,36 @@ if vim.g.loaded_phenix_session then
 end
 vim.g.loaded_phenix_session = true
 
+local Frontend = require("phenix.frontend")
+local config = Frontend.config("session", {
+  resession = {},
+  autosave = true,
+  restore_cursor = true,
+})
+
 local resession = require("resession")
-resession.setup({})
+resession.setup(config.resession)
 local feature = require("phenix.features.session")
-require("phenix.frontend").provide("session", feature, {
+Frontend.register_api("session", feature, {
   contract = { pick = "function", save = "function", load = "function" },
 })
 
-vim.api.nvim_create_autocmd("VimLeavePre", {
-  callback = function()
-    feature.save()
-  end,
-})
+if config.autosave then
+  vim.api.nvim_create_autocmd("VimLeavePre", {
+    callback = function()
+      feature.save()
+    end,
+  })
+end
 
-vim.api.nvim_create_autocmd("BufReadPost", {
-  callback = function()
-    local mark = vim.api.nvim_buf_get_mark(0, '\"')
-    local count = vim.api.nvim_buf_line_count(0)
-    if mark[1] > 0 and mark[1] <= count then
-      vim.api.nvim_win_set_cursor(0, mark)
-    end
-  end,
-})
+if config.restore_cursor then
+  vim.api.nvim_create_autocmd("BufReadPost", {
+    callback = function()
+      local mark = vim.api.nvim_buf_get_mark(0, '\"')
+      local count = vim.api.nvim_buf_line_count(0)
+      if mark[1] > 0 and mark[1] <= count then
+        vim.api.nvim_win_set_cursor(0, mark)
+      end
+    end,
+  })
+end
