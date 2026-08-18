544f209cbd66ef4d2491740463373c4c45f30949
Matthis Kälble <69082876+matthis-k@users.noreply.github.com>
2026-08-18 04:29:07 +0200

    refactor: publish theme module directly

--- pack/phenix/opt/phenix-theme/plugin/zz-phenix-theme-api.lua
+++ pack/phenix/opt/phenix-theme/plugin/zz-phenix-theme-api.lua
@@ -1,7 +1,5 @@
-require("phenix.frontend").register_api("theme", {
+require("phenix.frontend").project_api("theme", {
   colors = function()
     return require("base16-colorscheme").colors
   end,
-}, {
-  contract = { colors = "function" },
 })
