7891a1843a2d5974243d8541634182030eca5fce
Matthis Kälble <69082876+matthis-k@users.noreply.github.com>
2026-08-18 03:00:31 +0200

    feat: install routing selection integration

--- plugin/phenix.lua
+++ plugin/phenix.lua
@@ -4,6 +4,7 @@ end
 vim.g.loaded_phenix = 1
 
 require("phenix.mappings").install_plug_mappings()
+require("phenix.routing_selection").install()
 local frontend = require("phenix.frontend")
 frontend.register_api("agent", require("phenix"), {
   contract = {
